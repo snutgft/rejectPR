72f26056d920f779b1ed3db32af16f13a2f96a5a
snutgft <99342140+snutgft@users.noreply.github.com>
2022-12-01 14:45:23 +0000

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,70 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+type GitHub = ReturnType<typeof github.getOctokit>;
+
+async function removeExistingApprovalsIfExist(client: GitHub, pr: any) {
+  // Get list of all reviews on a PR
+  const { data: listReviews } = await client.rest.pulls.listReviews({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: pr.number,
+  });
+
+  // Get list of all commits to the PR
+  const { data: listCommits } = await client.rest.pulls.listCommits({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: pr.number,
+  });
+
+  // List logins of all commit authors on the PR
+  var commitAuthorLogins = listCommits.map(function (commit) {
+    return commit.author?.login;
+  });
+
+  // Remove PR approvals by any committer to the PR
+  for (let review of listReviews) {
+    if (
+      review.state === 'APPROVED' &&
+      review.user &&
+      commitAuthorLogins.includes(review.user.login)
+    ) {
+      core.info(
+        `Removing an approval (${review.id}) from ${review.user?.login} (cannot approve this PR since they committed to it)`
+      );
+      const dismissResponse = await client.rest.pulls.dismissReview({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pr.number,
+        review_id: review.id,
+        message: `${review.user?.login} cannot approve this PR since they committed to it`,
+      });
+      core.debug(`dismissResponse: ${JSON.stringify(dismissResponse)}`);
+      core.setFailed(
+        `${review.user?.login} cannot approve this PR since they committed to it`
+      );
+    }
+  }
+}
+
+async function run() {
+  try {
+    const token = core.getInput('github-token', { required: true });
+
+    const { pull_request: pr } = github.context.payload;
+    if (!pr) {
+      throw new Error(
+        'Event payload missing `pull_request` - workflow containing this action is supposed to be triggered by `pull_request` or `pull_request_target` event'
+      );
+    }
+
+    const client = github.getOctokit(token);
+
+    await removeExistingApprovalsIfExist(client, pr);
+  } catch (error) {
+    core.setFailed((error as Error).message);
+  }
+}
+
+run();
